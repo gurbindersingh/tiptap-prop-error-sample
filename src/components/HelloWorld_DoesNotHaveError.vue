4866f763613beed543739ab43e36498d0e025c5e
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2022-07-08 17:14:55 +0200

    Create sample

---
+++ src/components/HelloWorld_DoesNotHaveError.vue
@@ -0,0 +1,13 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script setup lang="ts">
+import { useEditor, EditorContent } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+
+const editor = useEditor({
+  content: "<h2>I do not throw an error 🎉.</h2>",
+  extensions: [StarterKit]
+});
+</script>
---
+++ src/components/HelloWorld_HasError.vue
@@ -0,0 +1,30 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script lang="ts">
+import { Editor, EditorContent } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    EditorContent
+  },
+
+  data() {
+    return {
+      editor: new Editor({
+        content: "<h2>I throw an error. 🐛</h2>",
+        extensions: [StarterKit]
+      })
+    };
+  },
+
+  mounted() {},
+
+  beforeUnmount() {
+    this.editor.destroy();
+  }
+});
+</script>
